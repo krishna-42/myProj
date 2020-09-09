f82639b6ed898fd138d1c4a49991fb0a519b6aec
krishna-42 <40002889+krishna-42@users.noreply.github.com>
2020-09-09 15:48:15 +0530

    Update python_exec1.py

--- python_exec1.py
+++ python_exec1.py
@@ -1,36 +1,42 @@
-
 import os
 
 #Import the necessary packages
 
-def sim_int(p,n,r):
-    p=int(input(""))
-    n=int(input(""))
-    r=int(input(""))
-    si =(p*n*r)/100
-    print(f"The Simple Interest is {si}")
-    print(dir())
-    return si
 
+def sim_int(p,n,r): 
+    p= int(input())
+    n= int(input())
+    r= int(input())
+    si = (p*n*r)/100
+    print("The Simple Interest is",si)
+    print(dir())
+
+    
+    
+    return si
+      
 import math
+
 def ceil_floor(a,b):
     a=eval(input(""))
     b=eval(input(""))
     print(math.ceil(a))
     print(math.floor(b))
+
+
     return a,b
 
 from math import sqrt
 def lists():
-    lst = []
-
+    lst =  []
     cnt_in=5
     while cnt_in >0:
         ele=float(input(""))
-        lst.append(round(sqrt(int(ele)),2))
+        lst.append(round(sqrt(ele),2))
         cnt_in -=1
     print(lst)
     print(type(ele))
+       
+           
     return lst
-
 # Driver code
